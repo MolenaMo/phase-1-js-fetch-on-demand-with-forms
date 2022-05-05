3831a16a6d0b1b1644ec8d1d5b42e4e6b46e2c41
MLM <100675603+MolenaMo@users.noreply.github.com>
2022-05-05 02:06:30 -0400

    Completed but doesn't work-faulty url-not my fault

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+    const inputForm = document.querySelector('form')
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const input = document.querySelector('input#searchByID');//instead of event.target.children[1].value
+        console.log(input.value)
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => { console.log(data)
+        let title = document.querySelector('section#movieDetails h4')
+        let summary = document.querySelector('section#movieDetails p')
+    
+         title.innerText = data.title
+         summary.innerText = data.summary
+   
+      })
+      
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', init)
